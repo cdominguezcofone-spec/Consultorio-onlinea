a930e06ba2bb1cc56fd65e4e129ba15d5775de67
cdccompany <cdominguezcofone@gmail.com>
2026-07-27 17:17:00 -0300

    Update URL for Google Apps Script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycby8J7l45AiYn1DUfIa-a7JsjabAY27IytuIICQpmutItiY7IoVBgaTt65SDhmuHlMIB/exec";
+  const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbzKsoJOQ4m9N5ulKn7_usjcKJzVDnn_b8HSIAqJGpwQBTQ7KNDh5QcackKbPe7AkSBZ/exec";
   const formTurno = document.getElementById("form-turno");
   const selectEspecialista = document.getElementById("especialista");
   const inputFecha = document.getElementById("fecha");
